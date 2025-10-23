6b3ab24e8ef33ad6e93c39f53bdb1153473b8d4a
Aiswarya r <aiswaryarathee123@gmail.com>
2025-10-23 16:41:04 +0530

    Add  test case for NeighbourTopEdge

--- life_test.go
+++ life_test.go
@@ -120,3 +120,35 @@ func TestNeighbourBottomrightCorner(t *testing.T) {
 		t.Error("Expected", Expected, "bt got", actual)
 	}
 }
+func TestNeighbourTopedge(t *testing.T) {
+	grid := NewGrid(4, 0, 0, 1, 0)
+	actual := CountAliveneighbours(0, 1, grid)
+	Expected := uint(1)
+	if actual != Expected {
+		t.Error("Expected", Expected, "bt got", actual)
+	}
+	grid = NewGrid(4, 0, 0, 1, 0, 1, 1)
+	actual = CountAliveneighbours(0, 1, grid)
+	Expected = uint(2)
+	if actual != Expected {
+		t.Error("Expected", Expected, "bt got", actual)
+	}
+	grid = NewGrid(4, 0, 0, 1, 0, 1, 1, 1, 2)
+	actual = CountAliveneighbours(0, 1, grid)
+	Expected = uint(3)
+	if actual != Expected {
+		t.Error("Expected", Expected, "bt got", actual)
+	}
+	grid = NewGrid(4, 0, 0, 1, 0, 1, 1, 1, 2, 0, 2)
+	actual = CountAliveneighbours(0, 1, grid)
+	Expected = uint(4)
+	if actual != Expected {
+		t.Error("Expected", Expected, "bt got", actual)
+	}
+	grid = NewGrid(4, 0, 0, 1, 0, 1, 1, 1, 2, 0, 2, 0, 1)
+	actual = CountAliveneighbours(0, 1, grid)
+	Expected = uint(5)
+	if actual != Expected {
+		t.Error("Expected", Expected, "bt got", actual)
+	}
+}
